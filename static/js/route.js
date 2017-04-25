73496057679a867b5c5916c3c3e902734dcf51a8
whobird <whobird@limeitingdeMacBook-Pro.local>
2017-04-25 13:47:31 +0800

    update all

--- static/js/route.js
+++ static/js/route.js
@@ -16,22 +16,36 @@ define(["js/app"],
                 //用于改变state时跳至顶部
                 $uiViewScrollProvider.useAnchorScroll();
                 // 默认进入先重定向
+
+                //$urlRouterProvider.otherwise('/main');
                 $urlRouterProvider.when('', '/main');
-                $urlRouterProvider.otherwise('/main');
                 $stateProvider
                     .state('main', {
                         //abstract: true,
                         url: '/main',
                         views:{
                             'content': {
-                                templateUrl: '../views/data_view.html',
+                                templateUrl: './views/data_view.html',
                                 controller:"dataCtrl",
                                 controllerAs:"dCtrl"
                             },
                             "right":{
-                                templateUrl: '../views/blank_right.html',
+                                templateUrl: './views/blank_right.html',
                             }
                         },
+                        resolve: {
+                           /* manageFeeData: function(manageFeeService) {
+                                return manageFeeService.getSetData();
+                            },
+                            data: ['$q','$timeout', function($q,$timeout){
+                                var defer = $q.defer();
+                                $timeout(function(){
+                                    defer.resolve();
+                                    amp_main.loading_hide();
+                                }, 300);
+                                return defer.promise;
+                            }]*/
+                        }
                     });
             });
     });
