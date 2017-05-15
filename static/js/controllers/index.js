f0d06449937a475c6a8d22d63d2e261d91cecc0d
whobird <whobird@limeitingdeMacBook-Pro.local>
2017-05-15 20:02:58 +0800

    update all

--- static/js/controllers/index.js
+++ static/js/controllers/index.js
@@ -3,6 +3,6 @@
  */
 //index起作用是导入器的作用，
 
-define(["./app.controllers","./mainController","./dataController","./menuController","./rightcontroller","./setMenuController","./setDataController","./setRightController","./modalController","./fileController"],function(){
+define(["./app.controllers","./mainController","./dataController","./menuController","./rightController","./setMenuController","./setDataController","./setRightController","./modalController"],function(){
 
 });
